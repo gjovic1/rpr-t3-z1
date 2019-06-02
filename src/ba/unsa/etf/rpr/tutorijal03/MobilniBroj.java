c2c24bc3aeb9d3cf67276b5b041938fea4487726
gjovic1 <gjovic1@etf.unsa.ba>
2019-06-02 12:49:32 +0200

    Urađen mobilni broj.

--- src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public class MobilniBroj {
+public class MobilniBroj extends TelefonskiBroj {
+    private int mobilnaMreza;
+    private String broj;
+
+    public MobilniBroj(int mobilnaMreza, String broj) throws IllegalArgumentException{
+        if(mobilnaMreza>67||mobilnaMreza<60) throw  new IllegalArgumentException("Neispravna mreza!");
+        this.mobilnaMreza=mobilnaMreza;
+        this.broj=broj;
+    }
+
+    @Override
+    public int hashCode(){
+        return 0;
+    }
+
+    @Override
+    public String ispisi(){
+        return "0"+mobilnaMreza+"+/"+broj;
+    }
 }
