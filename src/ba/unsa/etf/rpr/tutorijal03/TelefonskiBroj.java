4e6606a396be4d91bd077f391471f9575c36355e
gjovic1 <gjovic1@etf.unsa.ba>
2019-06-02 12:24:31 +0200

    Obavljena funkcionalnost TelefonskiBroj klase.

--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public class TelefonskiBroj {
+public abstract class TelefonskiBroj implements Comparable<TelefonskiBroj> {
+    public abstract String ispisi();
+
+    @Override
+    public abstract int hashCode();
+
+    @Override
+    public int compareTo(TelefonskiBroj telefonskiBroj){
+        return this.ispisi().compareTo(telefonskiBroj.ispisi());
+    }
 }
